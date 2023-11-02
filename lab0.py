ea0d52ede1fe8075161b730ebd923d64ca571157
GusyLebedy <gusevskaya.arina@mail.ru>
2023-11-02 12:18:36 +0300

    py update

--- lab0.py
+++ lab0.py
@@ -3,9 +3,8 @@ import pandas as pd
 df = pd.read_csv('data.csv', index_col='region_name')
 df.info()
 
-# Представим ситуацию, что из-за невнимательности операциониста, регионы: 
-# Республика Алтай, Магаданская обл. оказались не представлены в итоговой сводке.
-df.drop(['Республика Алтай', 'Магаданская обл.'], inplace=True)
+
+df.drop(['1', '2'], inplace=True)
 df.sort_values('salary', inplace=True)
 
 print(df.head(6).tail(1))
